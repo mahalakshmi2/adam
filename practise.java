7e47756ce00702367e71515212dffef0fe5dae11
maha <mahahalakshmi.dms@gamil.com>
2017-12-24 11:15:24 +0530

    commiting pratice file to local repo

---
+++ practise.java
@@ -0,0 +1,69 @@
+cd sai
+ls
+ls -a
+cd
+cd adam
+ls
+remove
+rm -rf all
+ls
+rm -rf task
+rm -rf README.md
+l
+ls
+ls -a
+git status
+git add .
+git status
+git commit -m "deleted the project task"
+ls
+git status
+git push origin master
+git status
+git remote
+git remote -v
+git branch -v
+git branch 
+git branch -v
+ls -a
+rm -rf .git
+ls -a
+mvn archetype:generate -DartifactId=maha -DgroupId=com.maha -Dversion=1.0-SNAPSHOT -Dinteracivemode=false
+ls
+cd maha
+ls
+git init
+ls
+ls -
+ls -a
+git status
+git add.
+git add .
+git commit -m "adding src file to local repo"
+git add.
+git status
+git remote
+git remote add bee https://github.com/mahalakshmi2/adam.git
+git remote
+git push bee master
+git push --all bee
+git pull
+git remote
+git pull bee
+git push --all bee
+ls
+git push origin master
+git push bee master
+git pull https://github.com/mahalakshmi2/adam.git
+git status
+git push bee master
+git remote -v
+git branch -v
+git branch
+history
+history -a practise.java
+ls
+vi practise.java
+git status
+history
+history -a practise.java
