d7982d6f162b977801e1a688475ba6f63fb09988
maha <mahahalakshmi.dms@gamil.com>
2017-12-27 00:03:07 +0530

    commiting new command to local

--- practise.java
+++ practise.java
@@ -67,3 +67,224 @@ vi practise.java
 git status
 history
 history -a practise.java
+ls
+uname -a
+uname -r
+uname -m
+uname -h
+uname --h
+uname -n
+hostname
+uname -s
+w
+who
+who -b
+uptime
+date
+date %m
+date '%m'
+date '+%m'
+date '+%b'
+date '+%B'
+date '+%y'
+date '+%Y'
+date '+%H'
+date '+%r'
+date '+%M'
+date '+%S'
+date '+%y'
+date '+%r'
+date '+%S'
+date '+%r'
+date '+%s'
+cat
+cal
+cal 2014
+cal 3 1992
+cal 2014
+cal 9 2014
+echo*
+echo *
+echo */n
+echo :*
+: echo *
+echo *
+echo 5
+echo *
+ls a-
+ls -a
+ls /
+ls .
+ls /home
+ls /home/maha
+ls /home
+ls -F
+ls -s
+ls -hs
+ls -h
+ls -s
+ls -r
+ls -l
+ls -lu
+ls -lf
+ls -lt
+ls -d ???
+ls -d ????
+ls -d ???
+ls -d ??
+ls -d ?
+ls -d ?????
+ls
+ls -d ??????
+ls -d ??????.??
+ls -d ????
+ls -d ???
+ls -d b???
+ls -d b ????
+ls
+ls -d *l
+ls -d *s
+ls 
+ls -d *a
+ls -d *c
+ls -d *t
+ls -d *d
+ls -d *m?
+ls
+ls -d
+man ls
+ls -f
+ls -F
+ls -d
+ls -d [sp]*
+ls -d [s-p]*
+ls -d [p-s]*
+ls -d .*
+ls -a
+ls -d .*
+ls -a
+ls -d .*
+ls -d .*!
+ls -d .*
+ls -d /etc/s*
+ls -i
+cat > 1.java
+cat < 1.java
+cat << 1.java
+hello
+
+cat > 1.java
+cat < 1.java
+cat > 1.java
+cat < 1.java
+cat >> 1.java
+cat < 1.java
+cat 1.java
+touch 2.java
+ls
+touch 1 2 3 4 {.java}
+ls
+rm .{java}
+rm {.java}
+ls
+rm 1 2 3
+ls
+rm 4
+ls
+touch [1 2 3].java
+ls
+rm [1 2 3].java
+ls
+touch [abc].java
+ls
+rm [abc].java
+ls
+touch ab{1,2,3,4}.java
+ls
+rm ab{1,2,3,4}.java
+ls
+mkdir -p /sai/maha/baby
+mkdir -p sai/maha/baby
+ls
+cd sai
+ls
+cd maha
+ls
+cd baby
+ls
+cd ..
+mkdir -p /a/b/c
+mkdir -p a/b/c
+ls
+cd a
+cd b
+cd c
+ls
+touch abc.java
+ls
+cd  
+cd adam
+cd maha
+ls
+cp 1.java 2.java
+ls
+cat 2.java
+cp 2.java sai
+ls
+cd sai
+ls
+cat 2.java
+cd ..
+cp -r sai a
+ls
+cd a
+ls
+cd sai
+ls
+cd maha
+ls
+cd baby
+ls
+cd ..
+ls
+cd ..
+cd ,,
+cd ..
+ls
+find -type d -name a/sai -exec rm -rf
+find -type d -name a/sai -exec rm -rf {}
+find -type d -name sai -exec rm -rf {}
+find -type d -name sai -exec rm -rf {}\;
+find -type d -name sai --exec rm -rf {}\;
+find -type d -name sai -exec rm -rf {}\;
+find -type d --name sai -exec rm -rf {}\;
+find -type d -name sai -exec rm -rf {}\;
+find -type d -name sai -exec rm -rf{}\;
+cd a
+ls
+rmdir sai
+rmdir -r  sai
+rmdir -rf  sai
+rm dir -rf  sai
+ls
+cd 
+cd adam 
+cd maha
+ls
+mv 1.java /sai/maha
+mv 1.java sai/maha
+ls
+cd sai
+cd maha
+ls
+cd ..
+l
+ls
+mv 2.java 3.java
+ls
+rm -r a
+ls
+touch 2.java
+ls
+ls -ltr
+history -a practise.java
