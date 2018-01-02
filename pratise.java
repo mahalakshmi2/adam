3d9d0d9cb76e9e1338b8cb482c1da5d982072d10
maha <mahahalakshmi.dms@gamil.com>
2018-01-02 18:09:04 +0530

    today history is saved

---
+++ pratise.java
@@ -0,0 +1,75 @@
+cd adam
+cd maha
+users
+last -a
+finger
+who -u
+whoami
+w
+w -h
+w -u
+who
+whoami
+id -un
+who a i
+who am i
+who mom likes
+last maha
+last ansible
+last ansible1
+just open a -a --all
+open a -a --all
+open a -a, --all
+who -a
+who -a 
+who -b
+who -q
+who --version
+man who
+who --all
+who --count
+count -q
+w -u -q
+
+w -u -q,
+who -uq
+who -q
+who --count
+who -p
+who -process
+who --process
+who -b
+uptime
+
+who --count
+find . -type f -u root /home/maha
+find . -type f -user root /home/maha
+find . -type f -user root 
+find / -type f -user root 
+find . -type f -time 7
+find . -type f -atime 7
+grep -i test
+grep -i test *
+grep -ilc test *
+grep -c test *
+grep -ilc test *
+grep -il test *
+grep -i test *| grep -l test *|grep -c test *
+find -size +2k
+find -size +2K
+find -size +2k
+find -size 2k
+find -size +2k
+top
+who
+who -q
+ls
+cat 2.java
+cat 3.java
+sed 's/hello/HELLO/' 3.java
+cat 3.java
+sed 's/hello/HELLO/g' 3.java
+cat 3.java
+history
+ls
+history -a pratise.java
